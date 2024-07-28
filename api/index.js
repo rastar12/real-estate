9c4afcd65a2a126f74c158c27c9757bc11d4eab3
rastar12 <eugenechanzu@gmail.com>
2024-07-28 11:33:09 +0300

    #join

--- api/index.js
+++ api/index.js
@@ -30,9 +30,9 @@ app.use('/api/listing',listingRouter);
 
 app.use(express.static(path.json(__dirname,'/client/dist')));
 
-app.get('*',(req,res)=>{
-    res.sendFile(path.join(__dirname,'client','dist','index.html'));
-})
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+  });
 
 app.use((err, req, res,next)=>{
     const statusCode=err.statuscode || 500;
